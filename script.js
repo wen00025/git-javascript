3f2790b139398ad4869207b22cdb830a24d65fe7
Mingjie Wen <wen00025@algonquinlive.com>
2020-01-13 16:50:02 -0500

    The JavaScript of Participation2

---
+++ script.js
@@ -0,0 +1,11 @@
+let name = "Mingjie"
+console.log(`Hello, my name is ${name}`)
+
+let number = 5
+let product = number * 2
+console.log(`The number ${number} multiplied by 2 is ${number * 2}`)
+console.log(`The number multiplied by 5 is ${product * 5}`)
+console.log(`The result divided by 5 is ${(product * 5) / number}`)
+
+let game = (product * 5) / number
+console.log(`The result subtract 7 is ${game - 7}`)
